ae6885aeb02a560d974123a6e36c108eff84f48f
Alex Newman <bakaev_a1998@mail.ru>
2022-12-05 14:53:50 +0300

    Added gRPC client application

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:5231");
+        var client = new Greeter.GreeterClient(channel);
+
+        try
+        {
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" },
+                deadline: DateTime.UtcNow.AddSeconds(8)
+            );
+
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+        catch (RpcException ex) when (ex.StatusCode == StatusCode.DeadlineExceeded)
+        {
+            Console.WriteLine("Timeout");
+        }
+    }
+}
