d622dd119992366ce41257b98d527a3ca29a37c5
Alex Newman <newman_98@mail.ru>
2022-12-05 17:12:36 +0300

    Added customers grpc service to server

--- GrpcApplication/Program.cs
+++ GrpcApplication/Program.cs
@@ -13,6 +13,7 @@ internal class Program
 
         // Configure the HTTP request pipeline.
         app.MapGrpcService<GreeterService>();
+        app.MapGrpcService<CustomersService>();
         app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
         app.Run();
---
+++ GrpcApplication/Services/CustomersService.cs
@@ -0,0 +1,55 @@
+﻿using Grpc.Core;
+
+namespace GrpcApplication.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private List<CustomerModel> _customers = new List<CustomerModel>()
+        {
+            new CustomerModel()
+            {
+                Id = "1",
+                FirstName = "Alex",
+                LastName = "Newman",
+                EmailAddress = "newman.alex@gmail.ru",
+                Age= 24
+            },
+            new CustomerModel()
+            {
+                Id = "2",
+                FirstName = "Nikola",
+                LastName = "Tesla",
+                EmailAddress = "nikola@gmail.ru",
+                Age= 25
+            },
+            new CustomerModel()
+            {
+                Id = "3",
+                FirstName = "Marisha",
+                LastName = "Anishkina",
+                EmailAddress = "amar@gmail.ru",
+                Age= 19
+            },
+            new CustomerModel()
+            {
+                Id = "4",
+                FirstName = "Nicole",
+                LastName = "Newman",
+                EmailAddress = "newman.nicole@gmail.ru",
+                Age= 22
+            },
+        };
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            var foundCustomer = _customers.FirstOrDefault(c => c.Id.Equals(request.UserId.ToString()));
+
+            if (foundCustomer == null)
+            {
+                return Task.FromResult(new CustomerModel());
+            }
+
+            return Task.FromResult(foundCustomer);
+        }
+    }
+}
